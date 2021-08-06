cdede5a771536a3cd98d697ee57aa005c197d913
edmundallan <mukwayaedmund53@gmail.com>
2021-08-06 20:50:51 +0300

    eccom

---
+++ includes/brands-slider.php
@@ -0,0 +1,58 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download (3).png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download (4).png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download.jpeg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download (2).png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/daikn.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download (5).png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/download (1).png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				
+
+
+
+		    </div>
+		</div>
+	
+</div>
---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>About Undria Fashions</h3>
+            </a>
+        </div>
+    
+    </div>
+
+     <div class="module-body m-t-20">
+        <p class="about-us"> Undria Fashions is a one stop online shop dealing in all sexes fashion wares and Accessories that keep you classy.</p>
+    
+        <div class="social-icons">
+            
+        <a href="http://facebook.com" class='active'><i class="icon fa fa-facebook"></i></a>
+        <a href="#"><i class="icon fa fa-twitter"></i></a>
+        <a href="#"><i class="icon fa fa-instagram"></i></a>
+
+        </div>
+    </div>
+
+</div>
+</div>
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening & Delivery time</h4>
+	</div>
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Monday-Friday:</td><td class="pull-right">08.00 To 6.00</td></tr>
+					<tr><td>Saturday:</td><td class="pull-right">09.00 To 4.00</td></tr>
+					<tr><td>Sunday:</td><td class="pull-right">07.00 To 12.00</td></tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+</div>
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">Contact Us</h4>
+	</div>
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                <p>Address: Kampala  <br>5th street,<br> plot 256 </p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>+256 777783304<br>+256 703400186</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="google.com">undriafashions.com</a></span><br>
+                    <span><a href="gmail.com">undriagraphics@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div>
+</div>
+</div>
+            </div>
+        </div>
+    </div>
+
+    
